4f39cadba45bfa973fa09c62467984e5297dfb04
isuru0 <isurushehara@gmail.com>
2024-02-29 10:45:05 +0530

    Add 'Recursion' package with 'Binary_Convert' class

---
+++ src/Recursion/Binary_Convert.java
@@ -0,0 +1,28 @@
+package Recursion;
+import java.util.Scanner;
+
+public class Binary_Convert {
+
+    //CREATE CONVERTOR FOR CONVERT DENARY NUMBERS TO BINARY NUMBERS
+    void convert(int number){
+        if(number > 0){
+            convert(number/2);
+            System.out.print(number%2);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        //CREATE OBJECTS
+        Binary_Convert converter = new Binary_Convert();
+        Scanner input = new Scanner(System.in);
+
+        //GET USER INPUT NUMBER
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        //CALL 'convert' METHOD WITH 'number' ARGUMENT
+        converter.convert(number);
+
+    }
+}
